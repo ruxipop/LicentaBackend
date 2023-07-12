9a913a45426bd0175e422fad73854d2ea3f62490
Doris Floris <teodoraoros.student@gmail.com>
2023-07-12 19:00:21 +0300

    kk

--- BackendLi/DataAccess/DesignTimeDbContextFactory.cs
+++ BackendLi/DataAccess/DesignTimeDbContextFactory.cs
@@ -5,6 +5,7 @@ namespace BackendLi.DataAccess;
 
 public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
 {
+    
     public DataContext CreateDbContext(string[] args)
     {
         return new DataContext("Server=eu-cdbr-west-03.cleardb.net;Database=heroku_b1ac73683ad42cb;Uid=bad24ad5363e70;Pwd=7fca4e2b;");
