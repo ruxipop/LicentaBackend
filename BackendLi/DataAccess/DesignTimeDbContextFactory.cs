e2e38cde551a09fb763446027da1f763d3241402
Doris Floris <teodoraoros.student@gmail.com>
2023-07-12 19:13:48 +0300

    c

--- BackendLi/DataAccess/DesignTimeDbContextFactory.cs
+++ BackendLi/DataAccess/DesignTimeDbContextFactory.cs
@@ -4,11 +4,9 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace BackendLi.DataAccess;
 
 public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
-
 {
-    
     public DataContext CreateDbContext(string[] args)
     {
-        return new DataContext("Server=eu-cdbr-west-03.cleardb.net;Database=heroku_b1ac73683ad42cb;Uid=bad24ad5363e70;Pwd=7fca4e2b;");
+        return new DataContext("server=localhost;database=vizo_db;user=root;password=root;PersistSecurityInfo=True");
     }
 }
